30e111b14dcb1b28cf496fd72aa9debdb93e6806
Fredrik Fossum <fredrik.fossum@gmail.com>
2019-12-05 07:59:48 +0100

    improve day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,9 @@
 use digits_iterator::*;
+use itertools::Itertools;
+use rayon::prelude::*;
 
 fn main() {
-    let range = || 254032..=789860;
+    let range = || (254032..=789860u32).into_par_iter();
 
     let part1_ans = range()
         .filter(has_monotonically_increasing_digits)
@@ -31,31 +33,8 @@ fn digit_pairs(x: &u32) -> impl Iterator<Item = (u8, u8)> {
 }
 
 fn has_strict_double_digit(x: &u32) -> bool {
-    let digits: Vec<u8> = x.digits().collect();
-
-    let starts_with_double = {
-        let first = digits.get(0);
-        let second = digits.get(1);
-        let third = digits.get(2);
-
-        first.is_some() && first == second && first != third
-    };
-
-    let ends_with_double = {
-        let last_i = digits.len() - 1;
-
-        let last = digits.get(last_i);
-        let second_last = digits.get(last_i - 1);
-        let third_last = digits.get(last_i - 2);
-
-        last.is_some() && last == second_last && last != third_last
-    };
-
-    let double_in_middle = {
-        digits.windows(4).any(|window| {
-            (window[0] != window[1]) && (window[1] == window[2]) && (window[2] != window[3])
-        })
-    };
-
-    starts_with_double || ends_with_double || double_in_middle
+    x.digits()
+        .group_by(|&x| x)
+        .into_iter()
+        .any(|(_key, grp)| grp.count() == 2)
 }
