d89a6482516d2b418f97ea525b7150ced103c6aa
Fredrik Fossum <fredrik.fossum@finn.no>
2019-12-12 13:28:24 +0100

    refactor intcode computer

--- src/day07.rs
+++ src/day07.rs
@@ -48,131 +48,159 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     ];
 
     let amplifiers = vec![0, 1, 2, 3, 4];
-    let res = amplifiers
-        .iter()
+    let part1_ans = amplifiers
+        .into_iter()
         .permutations(5)
         .filter_map(|sequence| run_amplifier_sequence(instructions.clone(), &sequence).ok())
-        .max();
+        .max()
+        .unwrap();
 
-    println!("{:?}", res);
+    println!("{}", part1_ans);
+
     Ok(())
 }
 
-fn run_amplifier_sequence(
-    instructions: Vec<i32>,
-    sequence: &[&i32],
-) -> Result<i32, Box<dyn Error>> {
-    let mut output = 0;
+fn run_amplifier_sequence(instructions: Vec<i32>, sequence: &[i32]) -> Result<i32, Box<dyn Error>> {
+    let mut previous_output = 0;
     for &x in sequence {
-        let outputs = run_program(&mut instructions.clone(), vec![output, *x])?;
-        output = outputs.first().copied().ok_or("expected input")?;
-    }
-    Ok(output)
-}
+        let instructions = instructions.clone();
+        let mut program = Program::new(instructions);
 
-fn get_opcode(instructions: &[i32], i: &mut usize) -> ([u8; 3], i32) {
-    let opcode: i32 = instructions[*i];
-    *i += 1;
+        program.run(Some(x))?;
+        let output = program.run(Some(previous_output))?;
 
-    let reversed_digits = opcode.digits().rev().collect::<Vec<u8>>();
-
-    let mode_1: u8 = reversed_digits.get(2).copied().unwrap_or(0);
-    let mode_2: u8 = reversed_digits.get(3).copied().unwrap_or(0);
-    let mode_3: u8 = reversed_digits.get(4).copied().unwrap_or(0);
-
-    let opcode = opcode % 100;
-
-    ([mode_1, mode_2, mode_3], opcode)
-}
-
-fn get_value(instructions: &[i32], i: &mut usize, mode: u8) -> Result<i32, Box<dyn Error>> {
-    let param = Parameter::new(mode, instructions[*i])?;
-    *i += 1;
-    Ok(param.get_value(instructions))
-}
-fn get_target(instructions: &[i32], i: &mut usize) -> Result<usize, Box<dyn Error>> {
-    let target = usize::try_from(instructions[*i])?;
-    *i += 1;
-    Ok(target)
-}
-
-fn run_program(
-    instructions: &mut Vec<i32>,
-    mut inputs: Vec<i32>,
-) -> Result<Vec<i32>, Box<dyn Error>> {
-    let mut i = 0;
-
-    let mut output = Vec::new();
-
-    while i < instructions.len() {
-        let ([mode_1, mode_2, _mode_3], opcode) = get_opcode(&instructions, &mut i);
-        match opcode {
-            1 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-                let target = get_target(&instructions, &mut i)?;
-                instructions[target] = value_1 + value_2;
-            }
-            2 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-                let target = get_target(&instructions, &mut i)?;
-                instructions[target] = value_1 * value_2;
-            }
-            3 => {
-                let target = get_target(&instructions, &mut i)?;
-                instructions[target] = inputs.pop().ok_or("no input to consume")?;
-            }
-            4 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                output.push(value_1);
-            }
-            5 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-
-                if value_1 != 0 {
-                    i = usize::try_from(value_2)?;
-                }
-            }
-            6 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-
-                if value_1 == 0 {
-                    i = usize::try_from(value_2)?;
-                }
-            }
-            7 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-                let target = get_target(&instructions, &mut i)?;
-
-                if value_1 < value_2 {
-                    instructions[target] = 1;
-                } else {
-                    instructions[target] = 0;
-                }
-            }
-            8 => {
-                let value_1 = get_value(&instructions, &mut i, mode_1)?;
-                let value_2 = get_value(&instructions, &mut i, mode_2)?;
-                let target = get_target(&instructions, &mut i)?;
-
-                if value_1 == value_2 {
-                    instructions[target] = 1;
-                } else {
-                    instructions[target] = 0;
-                }
-            }
-
-            99 => {
-                break;
-            }
-            x => Err(format!("illegal opcode: {}", x))?,
+        if let Output::Value(value) = output {
+            previous_output = value;
         }
     }
-    Ok(output)
+    Ok(previous_output)
+}
+
+struct Program {
+    instructions: Vec<i32>,
+    idx: usize,
+}
+
+impl Program {
+    pub fn new(instructions: Vec<i32>) -> Self {
+        Program {
+            instructions,
+            idx: 0,
+        }
+    }
+
+    fn get_opcode(&mut self) -> ([u8; 3], i32) {
+        let opcode: i32 = self.instructions[self.idx];
+        self.idx += 1;
+
+        let reversed_digits = opcode.digits().rev().collect::<Vec<u8>>();
+
+        let mode_1: u8 = reversed_digits.get(2).copied().unwrap_or(0);
+        let mode_2: u8 = reversed_digits.get(3).copied().unwrap_or(0);
+        let mode_3: u8 = reversed_digits.get(4).copied().unwrap_or(0);
+
+        let opcode = opcode % 100;
+
+        ([mode_1, mode_2, mode_3], opcode)
+    }
+
+    fn get_value(&mut self, mode: u8) -> Result<i32, Box<dyn Error>> {
+        let param = Parameter::new(mode, self.instructions[self.idx])?;
+        self.idx += 1;
+        Ok(param.get_value(&self.instructions))
+    }
+    fn get_address(&mut self) -> Result<usize, Box<dyn Error>> {
+        let target = usize::try_from(self.instructions[self.idx])?;
+        self.idx += 1;
+        Ok(target)
+    }
+
+    pub fn run(&mut self, mut input: Option<i32>) -> Result<Output, Box<dyn Error>> {
+        while self.idx < self.instructions.len() {
+            let instruction_start_i = self.idx;
+
+            let ([mode_1, mode_2, _mode_3], opcode) = self.get_opcode();
+            match opcode {
+                1 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+                    let target = self.get_address()?;
+                    self.instructions[target] = value_1 + value_2;
+                }
+                2 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+                    let target = self.get_address()?;
+                    self.instructions[target] = value_1 * value_2;
+                }
+                3 => {
+                    if let Some(input_value) = input.take() {
+                        let target = self.get_address()?;
+                        self.instructions[target] = input_value;
+                    } else {
+                        self.idx = instruction_start_i;
+                        return Ok(Output::WaitingForInput);
+                    }
+                }
+                4 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    return Ok(Output::Value(value_1));
+                }
+                5 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+
+                    if value_1 != 0 {
+                        self.idx = usize::try_from(value_2)?;
+                    }
+                }
+                6 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+
+                    if value_1 == 0 {
+                        self.idx = usize::try_from(value_2)?;
+                    }
+                }
+                7 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+                    let target = self.get_address()?;
+
+                    if value_1 < value_2 {
+                        self.instructions[target] = 1;
+                    } else {
+                        self.instructions[target] = 0;
+                    }
+                }
+                8 => {
+                    let value_1 = self.get_value(mode_1)?;
+                    let value_2 = self.get_value(mode_2)?;
+                    let target = self.get_address()?;
+
+                    if value_1 == value_2 {
+                        self.instructions[target] = 1;
+                    } else {
+                        self.instructions[target] = 0;
+                    }
+                }
+
+                99 => {
+                    return Ok(Output::Halted);
+                }
+                x => Err(format!("illegal opcode: {}", x))?,
+            }
+        }
+
+        Err("unexpected end")?
+    }
+}
+
+#[derive(Debug)]
+enum Output {
+    Value(i32),
+    WaitingForInput,
+    Halted,
 }
 
 #[cfg(test)]
@@ -185,7 +213,8 @@ mod tests {
             3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0,
         ];
         let sequence = vec![4, 3, 2, 1, 0];
-        let res = run_amplifier_sequence(instructions, sequence).unwrap();
+
+        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
         assert_eq!(res, 43210);
     }
     #[test]
@@ -195,7 +224,7 @@ mod tests {
             99, 0, 0,
         ];
         let sequence = vec![0, 1, 2, 3, 4];
-        let res = run_amplifier_sequence(instructions, sequence).unwrap();
+        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
         assert_eq!(res, 54321);
     }
 
@@ -206,7 +235,7 @@ mod tests {
             33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0,
         ];
         let sequence = vec![1, 0, 4, 3, 2];
-        let res = run_amplifier_sequence(instructions, sequence).unwrap();
+        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
         assert_eq!(res, 65210);
     }
 }
