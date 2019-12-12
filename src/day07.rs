95262e6d896ac25a2d021e8fa657615a9a6bb044
Fredrik Fossum <fredrik.fossum@finn.no>
2019-12-12 17:27:21 +0100

    complete day 7

--- src/day07.rs
+++ src/day07.rs
@@ -47,20 +47,53 @@ pub fn main() -> Result<(), Box<dyn Error>> {
         3, 9, 1002, 9, 2, 9, 4, 9, 99,
     ];
 
-    let amplifiers = vec![0, 1, 2, 3, 4];
+    let amplifiers = 0..=4;
     let part1_ans = amplifiers
-        .into_iter()
         .permutations(5)
-        .filter_map(|sequence| run_amplifier_sequence(instructions.clone(), &sequence).ok())
+        .filter_map(|sequence| run_amplifier_sequence(&instructions, &sequence).ok())
         .max()
         .unwrap();
 
     println!("{}", part1_ans);
 
+    let amplifiers = 5..=9;
+
+    let part2_ans = amplifiers
+    .permutations(5)
+    .filter_map(|sequence| run_looped_amplifier_sequence(&instructions, &sequence).ok())
+    .max()
+    .unwrap();
+
+    println!("{}", part2_ans);
+
     Ok(())
 }
 
-fn run_amplifier_sequence(instructions: Vec<i32>, sequence: &[i32]) -> Result<i32, Box<dyn Error>> {
+fn run_looped_amplifier_sequence(instructions: &Vec<i32>, phase_settings: &[i32]) -> Result<i32, Box<dyn Error>> {
+
+    let mut programs = Vec::new();
+    for &phase_setting in phase_settings {
+        let mut program = Program::new(instructions.clone());
+        program.run(Some(phase_setting))?;
+        programs.push(program)
+    }
+
+    let mut previous_output = 0;
+    for amp_id in (0..phase_settings.len()).cycle() {
+        let program = programs.get_mut(amp_id).unwrap();
+        let output = program.run(Some(previous_output))?;
+
+        match output {
+            Output::Value(value) => previous_output = value,
+            Output::Halted => return Ok(previous_output),
+            _ => ()
+        }
+    }
+
+    Err("no result")?
+}
+
+fn run_amplifier_sequence(instructions: &Vec<i32>, sequence: &[i32]) -> Result<i32, Box<dyn Error>> {
     let mut previous_output = 0;
     for &x in sequence {
         let instructions = instructions.clone();
@@ -93,11 +126,11 @@ impl Program {
         let opcode: i32 = self.instructions[self.idx];
         self.idx += 1;
 
-        let reversed_digits = opcode.digits().rev().collect::<Vec<u8>>();
+        let mut reversed_digits = opcode.digits().rev().skip(2);
 
-        let mode_1: u8 = reversed_digits.get(2).copied().unwrap_or(0);
-        let mode_2: u8 = reversed_digits.get(3).copied().unwrap_or(0);
-        let mode_3: u8 = reversed_digits.get(4).copied().unwrap_or(0);
+        let mode_1: u8 = reversed_digits.next().unwrap_or(0);
+        let mode_2: u8 = reversed_digits.next().unwrap_or(0);
+        let mode_3: u8 = reversed_digits.next().unwrap_or(0);
 
         let opcode = opcode % 100;
 
@@ -214,7 +247,7 @@ mod tests {
         ];
         let sequence = vec![4, 3, 2, 1, 0];
 
-        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
+        let res = run_amplifier_sequence(&instructions, &sequence).unwrap();
         assert_eq!(res, 43210);
     }
     #[test]
@@ -224,7 +257,7 @@ mod tests {
             99, 0, 0,
         ];
         let sequence = vec![0, 1, 2, 3, 4];
-        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
+        let res = run_amplifier_sequence(&instructions, &sequence).unwrap();
         assert_eq!(res, 54321);
     }
 
@@ -235,7 +268,18 @@ mod tests {
             33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0,
         ];
         let sequence = vec![1, 0, 4, 3, 2];
-        let res = run_amplifier_sequence(instructions, &sequence).unwrap();
+        let res = run_amplifier_sequence(&instructions, &sequence).unwrap();
         assert_eq!(res, 65210);
+    }
+
+    #[test]
+    fn test_loop_input_1() {
+        let instructions = vec![
+            3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+            27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5
+        ];
+        let sequence = vec![9,8,7,6,5];
+        let res = run_looped_amplifier_sequence(&instructions, &sequence).unwrap();
+        assert_eq!(res, 139629729);
     }
 }
