d0f77f965ac5388fa5d297198dcfdd328ee21121
Fredrik Fossum <fredrik.fossum@gmail.com>
2019-12-19 23:36:05 +0100

    day 17 part 1

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ mod day09;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
 
 fn main() {
-    let res = day14::main();
+    let res = day17::main();
     println!("{:?}", res);
 }
